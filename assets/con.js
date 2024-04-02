86c1e33872eadd78db1ba4531a23887aaf7247a9
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-04-02 09:21:22 +0300

    add constructor

---
+++ assets/con.js
@@ -0,0 +1,18 @@
+//  function Book(title,name){
+    // this.name = name;
+    // this.title = title;
+
+//  }
+
+//  const book = new Book ('The founder' ,'Nelson');
+
+//  console.log(book.title);
+
+ function Book (title ,author ,pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+ }
+
+ const book = new Book ('The Founder', 'Nelson' ,'278 pages');
+ 
